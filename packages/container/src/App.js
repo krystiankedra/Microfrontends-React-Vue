bc37445211e3a2fd849d2651130b6c68049d9235
Kedra, Krystian <krystian.kedra@atos.net>
2021-04-20 12:14:17 +0200

    Prepared authentication for user, fixed problem with initial pathname, configuration for communication beetwen apps for navigation and handlers

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,10 +1,35 @@
-import React from 'react';
-import MarketingApp from './components/MarketingApp';
+import React, { lazy, Suspense, useState } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { createGenerateClassName, StylesProvider } from '@material-ui/core/styles';
+
+import Header from './components/Header';
+import Progress from './components/Progress';
+
+const MarketingAppLazy = lazy(() => import('./components/MarketingApp'));
+const AuthAppLazy = lazy(() => import('./components/AuthApp'));
+
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'container',
+})
 
 export default () => {
+    const [isSignedIn, setIsSignedIn] = useState(false);
+
     return (
-        <div>
-            <MarketingApp />
-        </div>
+        <BrowserRouter>
+            <StylesProvider generateClassName={generateClassName}>
+                <div>
+                    <Header isSignedIn={isSignedIn} onSignOut={() => setIsSignedIn(false)} />
+                    <Suspense fallback={<Progress />}>
+                        <Switch>
+                            <Route path="/auth">
+                                <AuthAppLazy onSignIn={() => setIsSignedIn(true)} />
+                            </Route>
+                            <Route path="/" component={MarketingAppLazy} />
+                        </Switch>
+                    </Suspense>
+                </div>
+            </StylesProvider>
+        </BrowserRouter>
     );
 };
